8ae0e860f28d06efc4b06b9d97ba0743fac36997
codesyariah122 <pujiermanto@gmail.com>
2023-12-25 23:51:46 +0700

    update update

--- Resources/views/Layouts/DefaultLayout.core.php
+++ Resources/views/Layouts/DefaultLayout.core.php
@@ -16,6 +16,24 @@
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <script src=" https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js "></script>
     <style>
+        #popup-modal {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            position: fixed;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            z-index: 60;
+            background: rgba(0, 0, 0, 0.5);
+        }
+
+        .modal-content {
+            width: 100%;
+            max-width: 480px; 
+            margin: 0 auto;
+        }
 
         .swiper-pagination {
             bottom: 80px!important;
@@ -72,6 +90,9 @@
 
             /* Responsive styles for smaller devices */
             @media only screen and (max-width: 767px) {
+                .modal-content {
+                    max-width: 80%;
+                }
                 .swiper-pagination {
                     bottom: 80px!important;
                 }
@@ -125,6 +146,30 @@
     <body class="bg-white">
         <!-- Container -->
         <main class="container px-0 mx-auto lg:p-0 p-0 max-w-screen-sm">
+
+            <div id="popup-modal" tabindex="-1" class="hidden">
+                <div class="modal-content flex p-0 w-full max-w-md max-h-full">
+                    <div class="absolute z-60 lg:-left-2 top-20 inset-0 px-0 m-0 sm:mx-auto  w-[380px]">
+                        <div class="max-w-[380px] px-0">
+                            <div class="relative">
+                                <button onclick="closeModal()" data-modal-hide="popup-modal" data-modal-target="popup-modal" type="button" class="w-10 h-10 rounded-full bg-white flex items-center justify-center absolute right-0 top-0">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                        <line x1="18" y1="6" x2="6" y2="18"></line>
+                                        <line x1="6" y1="6" x2="18" y2="18"></line>
+                                    </svg>
+                                </button>
+                                <a href="#">
+                                    <div class="w-full">
+                                        <img src="https://amalsholeh-s3.imgix.net/user-media/ULW4Bw8aNrrx0ex7agyjDprymYoLqVWzQPWMU3MC.png?" class="w-full" alt="" width="534" height="300">
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
             <section id="layout-wrapper" class="p-4 rounded max-w-lg mx-auto">
                 <?php foreach ($partials as $partial) : ?>
                     <?php require_once "Resources/views/Layouts/" . $partial . ".core.php" ?>
@@ -141,6 +186,12 @@
         <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
         <script>
             feather.replace();
+            const modalElement = $('#popup-modal');
+
+            function closeModal() {
+                console.log("Clicked")
+                modalElement.hide('slow').fadeOut(1000)
+            }
         </script>
     </body>
 
